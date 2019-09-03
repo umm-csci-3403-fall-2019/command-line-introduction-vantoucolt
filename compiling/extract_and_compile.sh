97a200baaa293985fc26ab51e9f79b1ed0f61d65
xion1547 <xion1547@morris.umn.edu>
2019-09-03 11:14:21 -0500

    Completes exercise one

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,7 +1,8 @@
-echo "The first argument is" $1
-echo "The second argument is" $2
-
+#echo "The first argument is" $1
+#echo "The second argument is" $2
 tmp_dir=$2
-echo $tmp_dir
-tar -xvzf NthPrime.tgz -C $tmp_dir
-gcc NthPrime/main.c NthPrime/nth_prime.c -o NthPrimeExecutable
+#echo $tmp_dir
+tar -xzf NthPrime.tgz -C $tmp_dir
+gcc $tmp_dir/NthPrime/main.c $tmp_dir/NthPrime/nth_prime.c -o $tmp_dir/NthPrime/NthPrime
+output=$($tmp_dir/NthPrime/NthPrime $1)
+echo $output
